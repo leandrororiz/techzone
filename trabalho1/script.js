e7b75fe81cc9a07270bae2348b7060aab00277cd
leandrororiz <leandrororiz5@gmail.com>
2024-10-23 21:26:37 -0300

    techzone

---
+++ trabalho1/script.js
@@ -0,0 +1,9 @@
+function contatoSuporte() {
+    alert("Você pode contatar o suporte pelo email: suporte@techzone.com.br");
+}
+
+
+function iniciarChat() {
+    alert("Iniciando o chat online...");
+    
+}
